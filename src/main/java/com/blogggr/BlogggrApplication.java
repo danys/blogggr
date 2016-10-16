ec1289c63f767af7c983efbbcdcef61db5e36f87
Daniel Sünnen <netfox2006@hotmail.com>
2016-10-16 23:22:39 +0200

    Initial commit

---
+++ src/main/java/com/blogggr/BlogggrApplication.java
@@ -0,0 +1,12 @@
+package com.blogggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogggrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogggrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/blogggr/BlogggrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogggr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogggrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
