fdc048fccbbeb795c0ebdd339e7df501483828bf
Daniel Sünnen <netfox2006@hotmail.com>
2016-11-13 13:29:59 +0100

    Constructor takes error message

--- back end/src/main/java/com/blogggr/exceptions/ResourceNotFoundException.java
+++ back end/src/main/java/com/blogggr/exceptions/ResourceNotFoundException.java
@@ -4,4 +4,8 @@ package com.blogggr.exceptions;
  * Created by Daniel Sunnen on 12.11.16.
  */
 public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
