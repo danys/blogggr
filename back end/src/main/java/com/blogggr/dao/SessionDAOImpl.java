5a590b149a37ea551cc88d1989d8756035b40e3b
Daniel Sünnen <netfox2006@hotmail.com>
2016-11-19 11:26:29 +0100

    Modified query attribute and added exception handler

--- back end/src/main/java/com/blogggr/dao/SessionDAOImpl.java
+++ back end/src/main/java/com/blogggr/dao/SessionDAOImpl.java
@@ -3,6 +3,7 @@ package com.blogggr.dao;
 import com.blogggr.entities.Session;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -21,7 +22,16 @@ public class SessionDAOImpl extends GenericDAOImpl<Session> implements SessionDA
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Session> query = cb.createQuery(Session.class);
         Root<Session> root = query.from(Session.class);
-        query.where(cb.equal(root.get("sessionhash"),sessionHash));
-        return entityManager.createQuery(query).getSingleResult();
+        query.where(cb.equal(root.get("sessionHash"),sessionHash));
+        try {
+            return entityManager.createQuery(query).getSingleResult();
+        }
+        catch(NoResultException e){
+            return null;
+        }
+        catch(Exception e){ //Catches all other exceptions of the entityManager
+            //TODO may change this catch structure
+            return null;
+        }
     }
 }
