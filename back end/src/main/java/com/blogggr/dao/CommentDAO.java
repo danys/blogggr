aa09735af25bb2be1851f476dcf64e24551c647b
Daniel Sünnen <netfox2006@hotmail.com>
2016-12-05 23:39:19 +0100

    Added DAO for comment resource

---
+++ back end/src/main/java/com/blogggr/dao/CommentDAO.java
@@ -0,0 +1,11 @@
+package com.blogggr.dao;
+
+import com.blogggr.entities.Comment;
+
+/**
+ * Created by Daniel Sunnen on 05.12.16.
+ */
+public interface CommentDAO extends GenericDAO<Comment> {
+
+
+}
