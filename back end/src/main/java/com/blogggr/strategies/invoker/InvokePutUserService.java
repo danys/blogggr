a9dc699ddc94253350949fbff1a79e3d959f1195
Daniel Sünnen <netfox2006@hotmail.com>
2016-12-11 22:06:10 +0100

    Added create user invoker

---
+++ back end/src/main/java/com/blogggr/strategies/invoker/InvokePutUserService.java
@@ -0,0 +1,59 @@
+package com.blogggr.strategies.invoker;
+
+import com.blogggr.exceptions.DBException;
+import com.blogggr.exceptions.NotAuthorizedException;
+import com.blogggr.exceptions.ResourceNotFoundException;
+import com.blogggr.requestdata.UserPostData;
+import com.blogggr.services.UserService;
+import com.blogggr.strategies.ServiceInvocationStrategy;
+import com.blogggr.strategies.validators.IdValidator;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by Daniel Sunnen on 11.12.16.
+ */
+public class InvokePutUserService implements ServiceInvocationStrategy {
+
+    private UserService userService;
+
+    public InvokePutUserService(UserService commentService){
+        this.userService = commentService;
+    }
+
+    @Override
+    public Object invokeService(Map<String,String> input, String body, Long userID) throws ResourceNotFoundException, NotAuthorizedException, DBException {
+        if (!input.containsKey(IdValidator.idName)){
+            return null;
+        }
+        String idStr = input.get(IdValidator.idName);
+        Long userResourceID;
+        try{
+            userResourceID = Long.parseLong(idStr);
+        }
+        catch(NumberFormatException e){
+            return null;
+        }
+        //Parse the body and perform the update of the associated record
+        ObjectMapper mapper = new ObjectMapper();
+        UserPostData userData;
+        try{
+            userData = mapper.readValue(body, UserPostData.class);
+        }
+        catch(JsonParseException e){
+            return null;
+        }
+        catch(JsonProcessingException e){
+            return null;
+        }
+        catch(IOException e){
+            return null;
+        }
+        userService.updateUser(userResourceID, userID, userData);
+        return null;
+    }
+}
