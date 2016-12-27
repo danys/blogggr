9eeef3750aab4cdbf01f3cd380802c58cbba3ee4
Daniel Sünnen <netfox2006@hotmail.com>
2016-12-27 16:15:19 +0100

    Adjusted json output filter

--- back end/src/main/java/com/blogggr/strategies/invoker/InvokeGetPostService.java
+++ back end/src/main/java/com/blogggr/strategies/invoker/InvokeGetPostService.java
@@ -4,6 +4,7 @@ import com.blogggr.entities.Post;
 import com.blogggr.exceptions.DBException;
 import com.blogggr.exceptions.NotAuthorizedException;
 import com.blogggr.exceptions.ResourceNotFoundException;
+import com.blogggr.json.JsonFilter;
 import com.blogggr.json.JsonTransformer;
 import com.blogggr.services.PostService;
 import com.blogggr.strategies.ServiceInvocationStrategy;
@@ -41,25 +42,30 @@ public class InvokeGetPostService implements ServiceInvocationStrategy {
         Post post = postService.getPostById(id, userID);
         if (post==null) throw new ResourceNotFoundException("Post not found!");
         //Filter fields of the post => return post directly
-        Map<String,Set<String>> filterMap = new HashMap<>();
+
+        Map<String, JsonFilter> filterMap = new HashMap<>();
         filterMap.put("postID",null);
         filterMap.put("shortTitle",null);
         filterMap.put("textBody",null);
         filterMap.put("timestamp",null);
         filterMap.put("title",null);
         filterMap.put("global",null);
-        Set<String> userFilter = new HashSet<>();
-        userFilter.add("userID");
-        userFilter.add("email");
-        userFilter.add("lastName");
-        userFilter.add("firstName");
+        Map<String, JsonFilter> userFilterMap = new HashMap<>();
+        userFilterMap.put("userID",null);
+        userFilterMap.put("email",null);
+        userFilterMap.put("lastName",null);
+        userFilterMap.put("firstName",null);
+        JsonFilter userFilter = new JsonFilter(userFilterMap);
         filterMap.put("user",userFilter);
-        Set<String> commentFilter = new HashSet<>();
-        commentFilter.add("commentID");
-        commentFilter.add("text");
-        commentFilter.add("timestamp");
+        Map<String, JsonFilter> commentFilterMap = new HashMap<>();
+        commentFilterMap.put("commentID",null);
+        commentFilterMap.put("text",null);
+        commentFilterMap.put("timestamp",null);
+        commentFilterMap.put("user",userFilter);
+        JsonFilter commentFilter = new JsonFilter(commentFilterMap);
         filterMap.put("commentsArray",commentFilter);
-        JsonNode node = JsonTransformer.filterFieldsOfTwoLevelObject(post,filterMap);
+        JsonFilter jsonFilter = new JsonFilter(filterMap);
+        JsonNode node = JsonTransformer.filterFieldsOfMultiLevelObject(post,jsonFilter);
         return node;
     }
 }
