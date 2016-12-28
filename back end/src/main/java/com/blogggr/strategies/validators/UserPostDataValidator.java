6fafbee52d4a379a63628967cecac7d435f21d89
Daniel Sünnen <netfox2006@hotmail.com>
2016-12-28 16:55:48 +0100

    Validator tolerance for null values

--- back end/src/main/java/com/blogggr/strategies/validators/UserPostDataValidator.java
+++ back end/src/main/java/com/blogggr/strategies/validators/UserPostDataValidator.java
@@ -19,18 +19,25 @@ public class UserPostDataValidator extends GenericValidator {
     }
 
     public static String validateUserData(UserPostData userData){
-        if (userData.getFirstName().length()<3 || userData.getLastName().length()<3 || userData.getPassword().length()<8) {
+        //Check firstname, lastname and password
+        if ((userData.getFirstName()!=null && userData.getFirstName().length()<3) ||
+                (userData.getLastName()!=null && userData.getLastName().length()<3) ||
+                (userData.getPassword()!=null && userData.getPassword().length()<8)) {
             return "First name and last name must have at least 3 characters. Password must have 8 characters!";
         }
-        if (!userData.getEmail().contains("@")) {
-            return "E-mail address does not validate!";
+        //Check email
+        if (userData.getEmail()!=null){
+            if (!userData.getEmail().contains("@")) {
+                return "E-mail address does not validate!";
+            }
+            int atIndex = userData.getEmail().indexOf("@");
+            int dotIndex = userData.getEmail().indexOf(".",atIndex);
+            if (dotIndex==-1) {
+                return "E-mail address does not validate!";
+            }
         }
-        int atIndex = userData.getEmail().indexOf("@");
-        int dotIndex = userData.getEmail().indexOf(".",atIndex);
-        if (dotIndex==-1) {
-            return "E-mail address does not validate!";
-        }
-        if (userData.getPasswordRepeat().compareTo(userData.getPassword())!=0) {
+        //Check provided passwords
+        if (userData.getPassword()!=null && userData.getPasswordRepeat()!=null && userData.getPasswordRepeat().compareTo(userData.getPassword())!=0) {
             return "Submitted passwords must match!";
         }
         return "";
