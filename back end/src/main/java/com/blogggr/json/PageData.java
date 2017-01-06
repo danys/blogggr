536ebf4a65b776bdf3dcfaa886e4c116bb887983
Daniel Sünnen <netfox2006@hotmail.com>
2017-01-06 23:58:04 +0100

    Added class for holding paging meta data

---
+++ back end/src/main/java/com/blogggr/json/PageData.java
@@ -0,0 +1,44 @@
+package com.blogggr.json;
+
+/**
+ * Created by Daniel Sunnen on 06.01.17.
+ */
+public class PageData {
+
+    private Long totalCount;
+    private Long pageCount;
+    private String next;
+    private String previous;
+
+    public Long getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Long totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Long getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(Long pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+}
