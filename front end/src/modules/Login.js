020d9c8b91ee94517ef38651f3a19a2137f7dad0
Daniel Sünnen <netfox2006@hotmail.com>
2017-02-24 23:59:06 +0100

    Login and signup high level pages

---
+++ front end/src/modules/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+import Page from '../components/Page'
+
+export default class Login extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <Page loggedin="false" main={<LoginForm/>} highlight="home" />
+        );
+    }
+}
---
+++ front end/src/modules/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SignupForm from '../components/SignupForm'
+import Page from '../components/Page'
+
+export default class Signup extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <Page loggedin="true" main={<SignupForm/>} highlight="home" />
+        );
+    }
+}
