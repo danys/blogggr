f354064d81d840fc1e450ff34ac70d0866a10229
Daniel Sünnen <netfox2006@hotmail.com>
2017-02-24 01:48:44 +0100

    Modified page composition

--- front end/src/modules/App.js
+++ front end/src/modules/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Welcome from '../components/Welcome'
 import Page from '../components/Page'
+import BlogHome from '../components/BlogHome'
 
 export default class App extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
     render(){
-        return (
-        <Page loggedin="false" main={<Welcome/>}/>
+        const isLoggedIn = this.props.loggedin ==='true' ? true : false;
+        let content;
+        if (isLoggedIn) content = (
+            <Page loggedin="true" main={<BlogHome/>} highlight="home"/>
         );
+        else content = (
+            <Page loggedin="false" main={<Welcome/>} highlight="home"/>
+        );
+        return content;
     }
 }
