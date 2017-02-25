e85b749ac9efcc4d5cece74b7e81be55a48b6823
Daniel Sünnen <netfox2006@hotmail.com>
2017-02-25 15:36:31 +0100

    Added login form

--- front end/src/components/LoginForm.js
+++ front end/src/components/LoginForm.js
@@ -1,34 +1,48 @@
 import React from 'react'
 
-const LoginForm = () => (
-<div className="row">
-    <div className="col-md-4 col-md-offset-4">
-        <div className="login-panel panel panel-default">
-            <div className="panel-heading">
-                <h3 className="panel-title">Please sign in</h3>
-            </div>
-            <div className="panel-body">
-                <form role="form">
-                    <fieldset>
-                        <div className="form-group">
-                            <input className="form-control" placeholder="E-mail" name="email" type="email" autofocus />
-                        </div>
-                        <div className="form-group">
-                            <input className="form-control" placeholder="Password" name="password" type="password" value="" />
-                        </div>
-                        <div className="checkbox">
-                            <label>
-                                <input name="remember" type="checkbox" value="Remember Me" />Remember Me
-                            </label>
-                        </div>
-                        <a href="/" className="btn btn-lg btn-success btn-block">Login</a>
-                        <a href="/" className="btn btn-sm btn-primary btn-block">Back</a>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-)
+export default class LoginForm extends React.Component {
 
-export default LoginForm
+    constructor(props){
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+    }
+
+    handleLoginClick(){
+        //
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-4 col-md-offset-4">
+                    <div className="login-panel panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Please sign in</h3>
+                        </div>
+                        <div className="panel-body">
+                            <form role="form">
+                                <fieldset>
+                                    <div className="form-group">
+                                        <input className="form-control" placeholder="E-mail" name="email" type="email"
+                                               autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input className="form-control" placeholder="Password" name="password"
+                                               type="password" value=""/>
+                                    </div>
+                                    <div className="checkbox">
+                                        <label>
+                                            <input name="remember" type="checkbox" value="Remember Me"/>Remember Me
+                                        </label>
+                                    </div>
+                                    <a onClick={this.handleLoginClick()} className="btn btn-lg btn-success btn-block">Login</a>
+                                    <a href="/" className="btn btn-sm btn-primary btn-block">Back</a>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
