c7c8464bcd8ccc84671adbde03aeeb9369dd6b4b
Daniel Sünnen <netfox2006@hotmail.com>
2017-02-25 16:13:32 +0100

    Separate vendor dependencies file in order to create a separate vendor bundle

---
+++ front end/src/vendor.js
@@ -0,0 +1,19 @@
+//Vendor imports
+
+//React
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//React router
+import { Router, Route, browserHistory } from 'react-router'
+
+//Redux
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+//jQuery
+import jQuery from 'jquery'
+
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styling/css/style.css'
