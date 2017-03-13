1147073774a840654b4ea6ee8f988b3046bb1a47
Daniel Sünnen <netfox2006@hotmail.com>
2017-03-13 16:53:44 +0100

    Moved top level routing component state into children

--- front end/src/modules/App.js
+++ front end/src/modules/App.js
@@ -1,31 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { connect } from 'react-redux'
 import Footer from '../components/Footer'
 
-export class App extends React.Component{
+export default class App extends React.Component{
 
     constructor(props){
         super(props);
-        const isLoggedIn = this.props.loggedin === 'true' ? true : false;
-        this.state = {
-            loggedin: isLoggedIn,
-            navHighlight: "home"
-        }
-    }
-
-    componentWillReceiveProps(nextProps){
-        const isLoggedIn = nextProps.loggedin === 'true' ? true : false;
-        this.setState({
-            loggedin: isLoggedIn,
-            navHighlight: nextProps.navHighlight
-        });
     }
 
     render(){
         return (
         <div>
-            <Navbar loggedin={this.state.loggedin} highlight={this.state.navHighlight}/>
+            <Navbar highlight={this.props.children.props.route.name} router={this.props.router}/>
             <div className="container">
                 {this.props.children}
                 <Footer />
@@ -34,16 +20,3 @@ export class App extends React.Component{
         );
     }
 }
-
-const mapStateToProps = (state) => ({
-    loggedin: state.loggedin
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    //
-})
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(App)
