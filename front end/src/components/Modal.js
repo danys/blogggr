d5761999e1ebab3b2fbf3d0ff7919cb6e355481d
Daniel Sünnen <netfox2006@hotmail.com>
2017-03-24 23:46:49 +0100

    Modal react component

---
+++ front end/src/components/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export class Modal extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+        <div id="errorModal" className="modal fade" tabIndex="-1" role="dialog">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 className="modal-title">{this.props.title}</h4>
+                    </div>
+                    <div className="modal-body">
+                        {this.props.body}
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
