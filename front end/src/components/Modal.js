ddf6083018a20347465bb2a4d7c641d3f1655679
Daniel Sünnen <netfox2006@hotmail.com>
2017-03-27 22:47:23 +0200

    Apply red green background correctly

--- front end/src/components/Modal.js
+++ front end/src/components/Modal.js
@@ -8,13 +8,12 @@ export class Modal extends React.Component {
 
     render(){
         const idAttr = {id: this.props.modalId};
-        const color = 'background-color:'+this.props.color;
-        const headerColorStyle = {style: color};
+        const headerColorStyle = {backgroundColor: this.props.color};
         return (
         <div {...idAttr} className="modal fade" tabIndex="-1" role="dialog">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
-                    <div className="modal-header" {...headerColorStyle}>
+                    <div className="modal-header" style={headerColorStyle}>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <h4 className="modal-title">{this.props.title}</h4>
                     </div>
