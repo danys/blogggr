7dba32f8952d9747fec5b286b028c77bfa89e7c0
Daniel Sünnen <netfox2006@hotmail.com>
2017-04-05 23:35:37 +0200

    HTTP GET with query string functionality added

--- front end/src/utils/ajax.js
+++ front end/src/utils/ajax.js
@@ -11,11 +11,24 @@ export function post(url, requestData, successHandler, errorHandler, headers={})
     });
 }
 
+function toURLString(obj){
+    let queryString = '';
+    for (let key in obj) {
+        if (obj[key]!=undefined && obj[key]!=null && obj[key]!=''){
+            queryString += key;
+            queryString += '=';
+            queryString += obj[key];
+            queryString += '&';
+        }
+    }
+    return queryString;
+}
+
 export function get(url, requestData, successHandler, errorHandler, headers={}) {
     jQuery.ajax({
         url: url,
         method: 'GET',
-        data: JSON.stringify(requestData),
+        data: toURLString(requestData),
         dataType: 'json',
         contentType: "application/json; charset=utf-8",
         headers: headers,
