49f62fa846749f7b1f9e7780bc447e1fa51fbb9c
Daniel Sünnen <netfox2006@hotmail.com>
2017-04-13 16:05:22 +0200

    Added front end routes to the spring config

---
+++ back end/src/main/java/com/blogggr/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.blogggr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Daniel Sunnen on 13.04.17.
+ */
+@Configuration
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/login").setViewName("forward:/index.html");
+        registry.addViewController("/signup").setViewName("forward:/index.html");
+    }
+}
