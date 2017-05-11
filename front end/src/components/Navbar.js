94d9934fd69ca3ff8066bbcbded7849ff33c1cdf
Daniel Sünnen <netfox2006@hotmail.com>
2017-05-11 23:20:13 +0200

    Simplified navbar code

--- front end/src/components/Navbar.js
+++ front end/src/components/Navbar.js
@@ -35,13 +35,15 @@ class Navbar extends React.Component{
 
     render(){
         let loginFunctionality = '';
+        const homeProps = this.state.highlight && this.state.highlight==='home'?{className:"active"}:null;
+        const friendsProps = this.state.highlight && this.state.highlight==='friends'?{className:"active"}:null;
+        const userProps = this.state.highlight && this.state.highlight==='user'?{className:"dropdown active"}:{className:"dropdown"};
         if (this.state.loggedin) {
-            if (this.state.highlight==='home') {
                 loginFunctionality = (
                     <ul className="nav navbar-top-links navbar-right">
-                        <li className="active"><a href="/">Home</a></li>
-                        <li><a href="/friends">Friends</a></li>
-                        <li className="dropdown">
+                        <li {...homeProps}><a href="/">Home</a></li>
+                        <li {...friendsProps}><a href="/friends">Friends</a></li>
+                        <li {...userProps}>
                             <a className="dropdown-toggle" data-toggle="dropdown" href="#">
                                 <i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
                             </a>
@@ -57,51 +59,6 @@ class Navbar extends React.Component{
                         </li>
                     </ul>
                 );
-            }
-            else if (this.state.highlight==='friends'){
-                loginFunctionality = (
-                    <ul className="nav navbar-top-links navbar-right">
-                        <li><a href="/">Home</a></li>
-                        <li className="active"><a href="/friends">Friends</a></li>
-                        <li className="dropdown">
-                            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
-                            </a>
-                            <ul className="dropdown-menu dropdown-user">
-                                <li><a href="/user"><i className="fa fa-user fa-fw"></i> User Profile</a>
-                                </li>
-                                <li><a href="/settings"><i className="fa fa-gear fa-fw"></i> Settings</a>
-                                </li>
-                                <li className="divider"></li>
-                                <li><a href="#" onClick={this.handleLogoutClick}><i className="fa fa-sign-out fa-fw"></i> Logout</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                );
-            }
-            else if (this.state.highlight==='user'){
-                loginFunctionality = (
-                    <ul className="nav navbar-top-links navbar-right">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/friends">Friends</a></li>
-                        <li className="dropdown active">
-                            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
-                            </a>
-                            <ul className="dropdown-menu dropdown-user">
-                                <li><a href="/user"><i className="fa fa-user fa-fw"></i> User Profile</a>
-                                </li>
-                                <li><a href="/settings"><i className="fa fa-gear fa-fw"></i> Settings</a>
-                                </li>
-                                <li className="divider"></li>
-                                <li><a href="#" onClick={this.handleLogoutClick}><i className="fa fa-sign-out fa-fw"></i> Logout</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                );
-            }
         }
         return (
         <nav className="navbar navbar-default navbar-static-top" role="navigation">
