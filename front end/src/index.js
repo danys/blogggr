9b1d91e2b77be81dff8e9943bcbf2ec6ec4fee3e
Daniel Sünnen <netfox2006@hotmail.com>
2017-05-11 23:20:49 +0200

    Added post route

--- front end/src/index.js
+++ front end/src/index.js
@@ -8,6 +8,7 @@ import {loadState, saveState} from './utils/localStorage';
 import App from './modules/App';
 import Login from './modules/Login';
 import {Signup} from './modules/Signup';
+import Post from './modules/Post';
 import BlogHome from './modules/BlogHome';
 import reducer from './reducers/index';
 
@@ -24,6 +25,7 @@ ReactDOM.render(
                 <IndexRoute component={BlogHome} name="home"/>
                 <Route path="/login" component={Login} name="login"/>
                 <Route path="/signup" component={Signup} name="signup"/>
+                <Route path="/posts/:postName" component={Post} name="post"/>
                 /* TODO: "friends", "user" path names to specify */
             </Route>
         </Router>
