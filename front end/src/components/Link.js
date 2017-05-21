5a021d9c3781983fb3c4118acfd0fc63255e1b84
Daniel Sünnen <netfox2006@hotmail.com>
2017-05-21 23:57:02 +0200

    Added Link component that enables navigation links without fully reloading pages

---
+++ front end/src/components/Link.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { browserHistory } from 'react-router';
+
+class Link extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    handleClick(event){
+        event.preventDefault();
+        if (this.props.onClick) this.props.onClick(event);
+        browserHistory.push(this.props.url);
+    }
+
+    render(){
+        const text = this.props.children?this.props.children:this.props.text?this.props.text:null;
+        const cssClass = this.props.cssClass?this.props.cssClass:null;
+        let props = jQuery.extend( true, {}, this.props);
+        if (this.props.text) delete props["text"];
+        if (this.props.url) delete props["url"];
+        if (this.props.onClick) delete props["onClick"];
+        if (this.props.cssClass) delete props["cssClass"];
+        return (
+            cssClass?
+                <a {...props} className={cssClass} onClick={this.handleClick.bind(this)} href={this.props.url}>{text}</a>
+                :
+                <a {...props} onClick={this.handleClick.bind(this)} href={this.props.url}>{text}</a>
+        )
+    }
+}
+
+export default Link;
+
+Link.propTypes = {
+    onClick: PropTypes.func,
+    text: PropTypes.string,
+    url: PropTypes.string,
+    cssClass: PropTypes.string
+};
