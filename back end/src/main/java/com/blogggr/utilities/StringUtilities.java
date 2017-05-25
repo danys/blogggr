e2f7a9c62d3ccb03600b1e17c0f5373a1fe1c2c4
Daniel Sünnen <netfox2006@hotmail.com>
2017-05-26 00:45:03 +0200

    Added method to assemble HTTP query strings

--- back end/src/main/java/com/blogggr/utilities/StringUtilities.java
+++ back end/src/main/java/com/blogggr/utilities/StringUtilities.java
@@ -1,5 +1,8 @@
 package com.blogggr.utilities;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Daniel Sunnen on 21.11.16.
  */
@@ -15,6 +18,20 @@ public class StringUtilities {
             if (chars[i]!=' ') {sb.append(chars[i]);len++;}
             else {sb.append('-');len++;}
             if (len>30) break;
+        }
+        return sb.toString();
+    }
+
+    public static String buildQueryStringFromListOfKVPairs(List<Map.Entry<String,String>> listKVs){
+        StringBuilder sb = new StringBuilder();
+        Map.Entry<String, String> entry;
+        for(int i=0;i<listKVs.size();i++){
+            entry = listKVs.get(i);
+            if (entry.getKey()!=null && entry.getKey().compareTo("null")!=0 &&
+                    entry.getValue()!=null && entry.getValue().compareTo("null")!=0) {
+                if (i!=0 && sb.length()>0) sb.append("&");
+                sb.append(entry.getKey()+"="+entry.getValue());
+            }
         }
         return sb.toString();
     }
