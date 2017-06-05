0e77f592ba4eba8f0b9cb9892b42c8b4d1be2791
Daniel Sünnen <netfox2006@hotmail.com>
2017-06-05 16:42:42 +0200

    Added XSS filter to bean configuration

--- back end/src/main/java/com/blogggr/config/AppConfig.java
+++ back end/src/main/java/com/blogggr/config/AppConfig.java
@@ -1,13 +1,15 @@
 package com.blogggr.config;
 
+import com.blogggr.filters.XSSFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.servlet.Filter;
 
 /**
  * Created by Daniel Sunnen on 17.10.16.
  */
 @Configuration
-@EnableTransactionManagement
 public class AppConfig {
 
     public static final String apiVersion = "1.0";
@@ -22,4 +24,18 @@ public class AppConfig {
     public static final long sessionValidityMillis = 1000*60*60*24; //one day: maximum validity of a session. Max also applies for extensions.
     public static final String headerAuthorizationKey = "authorization";
     public static final int maxPostBodyLength = 100;
+
+    @Bean
+    public FilterRegistrationBean filterRegistration() {
+        FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+        filterRegBean.setFilter(xssFilter());
+        filterRegBean.addUrlPatterns("/*");
+        filterRegBean.setName("xssFilter");
+        filterRegBean.setOrder(1);
+        return filterRegBean;
+    }
+
+    public Filter xssFilter(){
+        return new XSSFilter();
+    }
 }
