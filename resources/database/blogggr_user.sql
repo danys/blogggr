e7a0391137ba242bbb8660cc09f74ff07156c57a
Daniel Sünnen <netfox2006@hotmail.com>
2017-06-22 14:59:08 +0200

    New database user only for the webapp

---
+++ resources/database/blogggr_user.sql
@@ -0,0 +1,7 @@
+create user blogggr with encrypted password 'blogggr123';
+grant usage on schema blogggr to blogggr;
+grant select on all tables in schema blogggr to blogggr;
+grant insert on all tables in schema blogggr to blogggr;
+grant update on all tables in schema blogggr to blogggr;
+grant usage on all sequences in schema blogggr to blogggr;
+grant select on all sequences in schema blogggr to blogggr;
