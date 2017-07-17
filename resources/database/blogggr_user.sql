ba516b4b03f024df299ebc7062433d1febb2a154
Daniel Sünnen <netfox2006@hotmail.com>
2017-07-17 23:05:00 +0200

    Added delete privilege

--- resources/database/blogggr_user.sql
+++ resources/database/blogggr_user.sql
@@ -3,5 +3,6 @@ grant usage on schema blogggr to blogggr;
 grant select on all tables in schema blogggr to blogggr;
 grant insert on all tables in schema blogggr to blogggr;
 grant update on all tables in schema blogggr to blogggr;
+grant delete on all tables in schema blogggr to blogggr;
 grant usage on all sequences in schema blogggr to blogggr;
 grant select on all sequences in schema blogggr to blogggr;
