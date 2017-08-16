02dd2a263143c47706b03c28e07ff5a97876b811
Daniel Sünnen <netfox2006@hotmail.com>
2017-08-16 23:38:00 +0200

    Added sequence

--- resources/database/create_db.sql
+++ resources/database/create_db.sql
@@ -1,9 +1,15 @@
 -- DDL
-
---DROP SCHEMA IF EXISTS blogggr CASCADE;
+-- object: blogggr | type: SCHEMA --
+-- DROP SCHEMA IF EXISTS blogggr CASCADE;
 CREATE SCHEMA blogggr;
+-- ddl-end --
 ALTER SCHEMA blogggr OWNER TO postgres;
+-- ddl-end --
+
 SET search_path TO pg_catalog,public,blogggr;
+-- ddl-end --
+
+-- object: blogggr.user_id_seq | type: SEQUENCE --
 -- DROP SEQUENCE IF EXISTS blogggr.user_id_seq CASCADE;
 CREATE SEQUENCE blogggr.user_id_seq
 	INCREMENT BY 1
@@ -13,8 +19,11 @@ CREATE SEQUENCE blogggr.user_id_seq
 	CACHE 1
 	NO CYCLE
 	OWNED BY NONE;
+-- ddl-end --
 ALTER SEQUENCE blogggr.user_id_seq OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.post_id_seq | type: SEQUENCE --
 -- DROP SEQUENCE IF EXISTS blogggr.post_id_seq CASCADE;
 CREATE SEQUENCE blogggr.post_id_seq
 	INCREMENT BY 1
@@ -24,9 +33,11 @@ CREATE SEQUENCE blogggr.post_id_seq
 	CACHE 1
 	NO CYCLE
 	OWNED BY NONE;
-
+-- ddl-end --
 ALTER SEQUENCE blogggr.post_id_seq OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.comment_id_seq | type: SEQUENCE --
 -- DROP SEQUENCE IF EXISTS blogggr.comment_id_seq CASCADE;
 CREATE SEQUENCE blogggr.comment_id_seq
 	INCREMENT BY 1
@@ -36,9 +47,11 @@ CREATE SEQUENCE blogggr.comment_id_seq
 	CACHE 1
 	NO CYCLE
 	OWNED BY NONE;
-
+-- ddl-end --
 ALTER SEQUENCE blogggr.comment_id_seq OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.friends | type: TABLE --
 -- DROP TABLE IF EXISTS blogggr.friends CASCADE;
 CREATE TABLE blogggr.friends(
 	user_one_id bigint,
@@ -49,24 +62,25 @@ CREATE TABLE blogggr.friends(
 	version bigint NOT NULL,
 
 );
-
+-- ddl-end --
 ALTER TABLE blogggr.friends OWNER TO postgres;
+-- ddl-end --
 
--- DROP TABLE IF EXISTS blogggr.user_images CASCADE;
-CREATE TABLE blogggr.user_images(
-	user_image_id bigint NOT NULL,
-	user_id bigint,
-	name varchar(64) NOT NULL,
-	is_current boolean NOT NULL,
-	width integer NOT NULL,
-	height integer NOT NULL,
-	version bigint NOT NULL,
-	CONSTRAINT user_image_image_id_pk PRIMARY KEY (user_image_id)
+-- object: blogggr.user_image_id_seq | type: SEQUENCE --
+-- DROP SEQUENCE IF EXISTS blogggr.user_image_id_seq CASCADE;
+CREATE SEQUENCE blogggr.user_image_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 999999999999999999
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
+-- ddl-end --
+ALTER SEQUENCE blogggr.user_image_id_seq OWNER TO postgres;
+-- ddl-end --
 
-);
-
-ALTER TABLE blogggr.user_images OWNER TO postgres;
-
+-- object: blogggr.post_image_id_seq | type: SEQUENCE --
 -- DROP SEQUENCE IF EXISTS blogggr.post_image_id_seq CASCADE;
 CREATE SEQUENCE blogggr.post_image_id_seq
 	INCREMENT BY 1
@@ -76,9 +90,11 @@ CREATE SEQUENCE blogggr.post_image_id_seq
 	CACHE 1
 	NO CYCLE
 	OWNED BY NONE;
-
+-- ddl-end --
 ALTER SEQUENCE blogggr.post_image_id_seq OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.comments | type: TABLE --
 -- DROP TABLE IF EXISTS blogggr.comments CASCADE;
 CREATE TABLE blogggr.comments(
 	comment_id bigint NOT NULL DEFAULT nextval('blogggr.comment_id_seq'::regclass),
@@ -90,9 +106,11 @@ CREATE TABLE blogggr.comments(
 	CONSTRAINT comments_commentid_pk PRIMARY KEY (comment_id)
 
 );
-
+-- ddl-end --
 ALTER TABLE blogggr.comments OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.posts | type: TABLE --
 -- DROP TABLE IF EXISTS blogggr.posts CASCADE;
 CREATE TABLE blogggr.posts(
 	post_id bigint NOT NULL DEFAULT nextval('blogggr.post_id_seq'::regclass),
@@ -107,9 +125,11 @@ CREATE TABLE blogggr.posts(
 	CONSTRAINT shorttitle_unique UNIQUE (short_title)
 
 );
-
+-- ddl-end --
 ALTER TABLE blogggr.posts OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.users | type: TABLE --
 -- DROP TABLE IF EXISTS blogggr.users CASCADE;
 CREATE TABLE blogggr.users(
 	user_id bigint NOT NULL DEFAULT nextval('blogggr.user_id_seq'::regclass),
@@ -128,9 +148,11 @@ CREATE TABLE blogggr.users(
 	CONSTRAINT email_unique UNIQUE (email)
 
 );
-
+-- ddl-end --
 ALTER TABLE blogggr.users OWNER TO postgres;
+-- ddl-end --
 
+-- object: blogggr.post_images | type: TABLE --
 -- DROP TABLE IF EXISTS blogggr.post_images CASCADE;
 CREATE TABLE blogggr.post_images(
 	post_image_id bigint NOT NULL DEFAULT nextval('blogggr.post_image_id_seq'::regclass),
@@ -142,60 +164,81 @@ CREATE TABLE blogggr.post_images(
 	CONSTRAINT post_image_id_pk PRIMARY KEY (post_image_id)
 
 );
-
+-- ddl-end --
 ALTER TABLE blogggr.post_images OWNER TO postgres;
+-- ddl-end --
 
--- DROP SEQUENCE IF EXISTS blogggr.user_image_id_seq CASCADE;
-CREATE SEQUENCE blogggr.user_image_id_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 999999999999999999
-	START WITH 1
-	CACHE 1
-	NO CYCLE
-	OWNED BY NONE;
+-- object: blogggr.user_images | type: TABLE --
+-- DROP TABLE IF EXISTS blogggr.user_images CASCADE;
+CREATE TABLE blogggr.user_images(
+	user_image_id bigint NOT NULL DEFAULT nextval('blogggr.user_image_id_seq'::regclass),
+	user_id bigint,
+	name varchar(64) NOT NULL,
+	is_current boolean NOT NULL,
+	width integer NOT NULL,
+	height integer NOT NULL,
+	version bigint NOT NULL,
+	CONSTRAINT user_image_image_id_pk PRIMARY KEY (user_image_id)
 
-ALTER SEQUENCE blogggr.user_image_id_seq OWNER TO postgres;
+);
+-- ddl-end --
+ALTER TABLE blogggr.user_images OWNER TO postgres;
+-- ddl-end --
 
+-- object: friends_useroneid_pk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.friends DROP CONSTRAINT IF EXISTS friends_useroneid_pk CASCADE;
 ALTER TABLE blogggr.friends ADD CONSTRAINT friends_useroneid_pk FOREIGN KEY (user_one_id)
 REFERENCES blogggr.users (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: friends_usertwoid_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.friends DROP CONSTRAINT IF EXISTS friends_usertwoid_fk CASCADE;
 ALTER TABLE blogggr.friends ADD CONSTRAINT friends_usertwoid_fk FOREIGN KEY (user_two_id)
 REFERENCES blogggr.users (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: last_action_user_id_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.friends DROP CONSTRAINT IF EXISTS last_action_user_id_fk CASCADE;
 ALTER TABLE blogggr.friends ADD CONSTRAINT last_action_user_id_fk FOREIGN KEY (last_action_user_id)
 REFERENCES blogggr.users (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: user_images_user_id_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.user_images DROP CONSTRAINT IF EXISTS user_images_user_id_fk CASCADE;
 ALTER TABLE blogggr.user_images ADD CONSTRAINT user_images_user_id_fk FOREIGN KEY (user_id)
 REFERENCES blogggr.users (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: comments_postid_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.comments DROP CONSTRAINT IF EXISTS comments_postid_fk CASCADE;
 ALTER TABLE blogggr.comments ADD CONSTRAINT comments_postid_fk FOREIGN KEY (post_id)
 REFERENCES blogggr.posts (post_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: comments_userid_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.comments DROP CONSTRAINT IF EXISTS comments_userid_fk CASCADE;
 ALTER TABLE blogggr.comments ADD CONSTRAINT comments_userid_fk FOREIGN KEY (user_id)
 REFERENCES blogggr.users (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: posts_userid_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.posts DROP CONSTRAINT IF EXISTS posts_userid_fk CASCADE;
 ALTER TABLE blogggr.posts ADD CONSTRAINT posts_userid_fk FOREIGN KEY (user_id)
 REFERENCES blogggr.users (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
 
+-- object: post_image_post_id_fk | type: CONSTRAINT --
 -- ALTER TABLE blogggr.post_images DROP CONSTRAINT IF EXISTS post_image_post_id_fk CASCADE;
 ALTER TABLE blogggr.post_images ADD CONSTRAINT post_image_post_id_fk FOREIGN KEY (post_id)
 REFERENCES blogggr.posts (post_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+-- ddl-end --
 
 
