f9023920c88599be6772b50a4daef90a8e6a8a5b
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-07 00:58:08 +0200

    Add UserDto

---
+++ back end/src/main/java/com/blogggr/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.blogggr.dto;
+
+import com.blogggr.entities.UserImage;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Daniel Sunnen on 07.06.18.
+ */
+@Getter
+@Setter
+public class UserDto {
+
+  private Long userId;
+  private String email;
+  private String firstName;
+  private String lastName;
+  private UserImage image;
+}
