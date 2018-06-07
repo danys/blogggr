3342a9ea49a73af09d9f6a5e1172190206c57f72
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-08 00:07:36 +0200

    Added userimagedto

---
+++ back end/src/main/java/com/blogggr/dto/UserImageDto.java
@@ -0,0 +1,16 @@
+package com.blogggr.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Daniel Sunnen on 08.06.18.
+ */
+@Getter
+@Setter
+public class UserImageDto {
+
+  private String name;
+  private Integer width;
+  private Integer height;
+}
