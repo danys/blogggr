ea6fba4c7f82a32829fbc14791fcc9b2da3002fa
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-10 16:25:33 +0200

    Simplified entities

--- back end/src/main/java/com/blogggr/entities/Comment.java
+++ back end/src/main/java/com/blogggr/entities/Comment.java
@@ -1,8 +1,5 @@
 package com.blogggr.entities;
 
-import com.blogggr.json.JsonTransformer;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.io.Serializable;
 import java.sql.Timestamp;
 import javax.persistence.Column;
@@ -15,6 +12,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
 
 
 /**
@@ -22,6 +21,8 @@ import javax.persistence.Version;
  */
 @Entity
 @Table(name = "comments", schema = "blogggr")
+@Getter
+@Setter
 public class Comment implements Serializable {
 
   private static final long serialVersionUID = 1L;
@@ -36,73 +37,14 @@ public class Comment implements Serializable {
 
   private Timestamp timestamp;
 
-  //bi-directional many-to-one association to Post
-  @JsonIgnore
   @ManyToOne
   @JoinColumn(name = "post_id")
   private Post post;
 
-  //bi-directional many-to-one association to User
   @ManyToOne
   @JoinColumn(name = "user_id")
   private User user;
 
   @Version
   private Long version;
-
-  public Comment() {
-  }
-
-  public Long getCommentId() {
-    return this.commentId;
-  }
-
-  public void setCommentId(Long commentId) {
-    this.commentId = commentId;
-  }
-
-  public String getText() {
-    return this.text;
-  }
-
-  public void setText(String text) {
-    this.text = text;
-  }
-
-  public String getTimestamp() {
-    return JsonTransformer.timestampToString(timestamp);
-  }
-
-  @JsonIgnore
-  public Timestamp getRealTimestamp() {
-    return timestamp;
-  }
-
-  public void setTimestamp(Timestamp timestamp) {
-    this.timestamp = timestamp;
-  }
-
-  public Post getPost() {
-    return this.post;
-  }
-
-  public void setPost(Post post) {
-    this.post = post;
-  }
-
-  public User getUser() {
-    return this.user;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
-  }
-
-  public Long getVersion() {
-    return version;
-  }
-
-  public void setVersion(Long version) {
-    this.version = version;
-  }
 }
--- back end/src/main/java/com/blogggr/entities/Post.java
+++ back end/src/main/java/com/blogggr/entities/Post.java
@@ -1,8 +1,5 @@
 package com.blogggr.entities;
 
-import com.blogggr.json.JsonTransformer;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
@@ -18,6 +15,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
 
 
 /**
@@ -25,6 +24,8 @@ import javax.persistence.Version;
  */
 @Entity
 @Table(name = "posts", schema = "blogggr")
+@Getter
+@Setter
 public class Post implements Serializable {
 
   private static final long serialVersionUID = 1L;
@@ -48,118 +49,16 @@ public class Post implements Serializable {
   @Column(name = "is_global")
   private Boolean isGlobal;
 
-  //bi-directional many-to-one association to Comment
   @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
   private List<Comment> comments;
 
-  //bi-directional many-to-one association to Post
   @OneToMany(mappedBy = "post")
   private List<PostImage> postImages;
 
-  //bi-directional many-to-one association to User
   @ManyToOne
   @JoinColumn(name = "user_id")
   private User user;
 
   @Version
   private Long version;
-
-  public Post() {
-  }
-
-  //Getters and setters
-
-  public String getTimestamp() {
-    return JsonTransformer.timestampToString(timestamp);
-  }
-
-  public Long getPostId() {
-    return this.postId;
-  }
-
-  public void setPostId(Long postId) {
-    this.postId = postId;
-  }
-
-  public String getShortTitle() {
-    return this.shortTitle;
-  }
-
-  public void setShortTitle(String shorttitle) {
-    this.shortTitle = shorttitle;
-  }
-
-  public String getTextBody() {
-    return this.textBody;
-  }
-
-  public void setTextBody(String textbody) {
-    this.textBody = textbody;
-  }
-
-  public void setTimestamp(Timestamp timestamp) {
-    this.timestamp = timestamp;
-  }
-
-  public String getTitle() {
-    return this.title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public Boolean getGlobal() {
-    return isGlobal;
-  }
-
-  public void setGlobal(Boolean global) {
-    isGlobal = global;
-  }
-
-  public List<Comment> getComments() {
-    return this.comments;
-  }
-
-  public void setComments(List<Comment> comments) {
-    this.comments = comments;
-  }
-
-  public Comment addComment(Comment comment) {
-    getComments().add(comment);
-    comment.setPost(this);
-
-    return comment;
-  }
-
-  public Comment removeComment(Comment comment) {
-    getComments().remove(comment);
-    comment.setPost(null);
-
-    return comment;
-  }
-
-  public User getUser() {
-    return this.user;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
-  }
-
-  public List<PostImage> getPostImages() {
-    return postImages;
-  }
-
-  public void setPostImages(List<PostImage> postImages) {
-    this.postImages = postImages;
-  }
-
-  public Long getVersion() {
-    return version;
-  }
-
-  public void setVersion(Long version) {
-    this.version = version;
-  }
 }
