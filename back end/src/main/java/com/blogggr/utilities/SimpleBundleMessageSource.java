acbb1477ea9621b729cb3e86200cdd2d6fa7048b
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-11 01:02:57 +0200

    Added class to easily get the requested message in the right locale

---
+++ back end/src/main/java/com/blogggr/utilities/SimpleBundleMessageSource.java
@@ -0,0 +1,19 @@
+package com.blogggr.utilities;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.lang.Nullable;
+
+/**
+ * Created by Daniel Sunnen on 11.06.18.
+ */
+public class SimpleBundleMessageSource extends ResourceBundleMessageSource{
+
+  public String getMessage(String code){
+    return getMessage(code, null, LocaleContextHolder.getLocale());
+  }
+
+  public String getMessage(String code, @Nullable Object[] args){
+    return getMessage(code, args, LocaleContextHolder.getLocale());
+  }
+}
