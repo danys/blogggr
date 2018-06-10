d68fb1ddc0e4a855dab1083de816cd825685cf81
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-10 16:32:47 +0200

    Mapper to convert entity objects to dto objects for api responses

--- back end/src/main/java/com/blogggr/utilities/DtoConverter.java
+++ back end/src/main/java/com/blogggr/utilities/DtoConverter.java
@@ -1,6 +1,13 @@
 package com.blogggr.utilities;
 
-import com.blogggr.dto.UserDto;
+import com.blogggr.dto.out.CommentDto;
+import com.blogggr.dto.out.FriendDto;
+import com.blogggr.dto.out.PostDto;
+import com.blogggr.dto.out.UserDto;
+import com.blogggr.dto.out.UserWithImageDto;
+import com.blogggr.entities.Comment;
+import com.blogggr.entities.Friend;
+import com.blogggr.entities.Post;
 import com.blogggr.entities.User;
 import org.modelmapper.ModelMapper;
 
@@ -19,5 +26,28 @@ public class DtoConverter {
     user.getImage();
     UserDto userDto = modelMapper.map(user, UserDto.class);
     return userDto;
+  }
+
+  public UserWithImageDto toUserWithImageDto(User user) {
+    user.getImage();
+    UserWithImageDto userWithImageDto = modelMapper.map(user, UserWithImageDto.class);
+    return userWithImageDto;
+  }
+
+  public PostDto toPostDto(Post post) {
+    PostDto postDto = modelMapper.map(post, PostDto.class);
+    return postDto;
+  }
+
+  public CommentDto toCommentDto(Comment comment){
+    CommentDto commentDto = modelMapper.map(comment, CommentDto.class);
+    return commentDto;
+  }
+
+  public FriendDto toFriendDto(Friend friend){
+    FriendDto friendDto = modelMapper.map(friend, FriendDto.class);
+    friendDto.setUserId1(friend.getUser1());
+    friendDto.setUserId2(friend.getUser2());
+    return friendDto;
   }
 }
