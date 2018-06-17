2c5510fc48e1bc5595b5e171d0b9f8b32a49c29c
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-17 16:20:10 +0200

    Throw handler not found exception and return custom response

--- back end/src/main/java/com/blogggr/Application.java
+++ back end/src/main/java/com/blogggr/Application.java
@@ -4,12 +4,18 @@ import com.blogggr.config.StorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageConfig.class)
+@EnableWebMvc
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+    dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
   }
 }
--- back end/src/main/java/com/blogggr/exceptions/CustomResponseEntityExceptionHandler.java
+++ back end/src/main/java/com/blogggr/exceptions/CustomResponseEntityExceptionHandler.java
@@ -8,13 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.NonTransientDataAccessException;
 import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.dao.TransientDataAccessResourceException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.datasource.init.ScriptException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -23,64 +28,81 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final Logger logging = LoggerFactory.getLogger(this.getClass());
 
   @Autowired
   private SimpleBundleMessageSource simpleBundleMessageSource;
 
-  @ExceptionHandler(value = Exception.class)
-  protected ResponseEntity handleGenericException(RuntimeException ex) {
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity handleGenericException(RuntimeException ex) {
     String message = simpleBundleMessageSource.getMessage("exception.other.error");
-    logger.error(message, ex);
+    logging.error(message, ex);
     return ResponseBuilder.errorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  @ExceptionHandler(value = IllegalArgumentException.class)
-  protected ResponseEntity handleIllegalArgumentException(RuntimeException ex) {
-    logger.error(ex.getMessage());
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity handleIllegalArgumentException(RuntimeException ex) {
+    logging.error(ex.getMessage());
     return ResponseBuilder.errorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
-  @ExceptionHandler(value = ResourceNotFoundException.class)
-  protected ResponseEntity handleResourceNotFoundException(RuntimeException ex) {
-    logger.error(ex.getMessage(), ex);
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity handleResourceNotFoundException(RuntimeException ex) {
+    logging.error(ex.getMessage(), ex);
     return ResponseBuilder.errorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
   }
 
-  @ExceptionHandler(value = UsernameNotFoundException.class)
-  protected ResponseEntity handleUsernameNotFoundException(RuntimeException ex) {
+  @ExceptionHandler(UsernameNotFoundException.class)
+  public ResponseEntity handleUsernameNotFoundException(RuntimeException ex) {
     String message = simpleBundleMessageSource.getMessage("exception.authentication.userNotFound");
-    logger.error(message, ex);
+    logging.error(message, ex);
     return ResponseBuilder.errorResponse(message, HttpStatus.UNAUTHORIZED);
   }
 
   //All Database exceptions of Repository beans translated by Spring into instances of DataAccessException
 
-  @ExceptionHandler(value = NonTransientDataAccessException.class)
-  protected ResponseEntity handleNonTransientDataAccessException(RuntimeException ex) {
+  @ExceptionHandler(NonTransientDataAccessException.class)
+  public ResponseEntity handleNonTransientDataAccessException(RuntimeException ex) {
     String message = simpleBundleMessageSource.getMessage("exception.db.nonTransientExceptionError");
-    logger.error(message, ex);
+    logging.error(message, ex);
     return ResponseBuilder.errorResponse(message, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  @ExceptionHandler(value = RecoverableDataAccessException.class)
-  protected ResponseEntity handleRecoverableDataAccessException(RuntimeException ex) {
+  @ExceptionHandler(RecoverableDataAccessException.class)
+  public ResponseEntity handleRecoverableDataAccessException(RuntimeException ex) {
     String message = simpleBundleMessageSource.getMessage("exception.db.recoverableExceptionError");
-    logger.error(message, ex);
+    logging.error(message, ex);
     return ResponseBuilder.errorResponse(message, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  @ExceptionHandler(value = ScriptException.class)
-  protected ResponseEntity handleScriptException(RuntimeException ex) {
+  @ExceptionHandler(ScriptException.class)
+  public ResponseEntity handleScriptException(RuntimeException ex) {
     String message = simpleBundleMessageSource.getMessage("exception.db.scriptExceptionError");
-    logger.error(message, ex);
+    logging.error(message, ex);
     return ResponseBuilder.errorResponse(message, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  @ExceptionHandler(value = TransientDataAccessResourceException.class)
-  protected ResponseEntity handleTransientDataAccessResourceException(RuntimeException ex) {
+  @ExceptionHandler(TransientDataAccessResourceException.class)
+  public ResponseEntity handleTransientDataAccessResourceException(RuntimeException ex) {
     String message = simpleBundleMessageSource.getMessage("exception.db.transientExceptionError");
-    logger.error(message, ex);
+    logging.error(message, ex);
     return ResponseBuilder.errorResponse(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  /**
+   * Custom response for NoHandlerFoundException.class which is treated in the parent class
+   * and delegates to this method
+   * @param ex
+   * @param headers
+   * @param status
+   * @param request
+   * @return
+   */
+  @Override
+  protected ResponseEntity<Object> handleNoHandlerFoundException(
+      NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    String message = simpleBundleMessageSource.getMessage("exception.controller.noHandlerFound");
+    logging.error(message + " Exception message: " + ex.getMessage());
+    return ResponseBuilder.errorResponse(message, HttpStatus.NOT_FOUND);
   }
 }
