6d001e459f394e1d28e1d64f3ea259075a21318b
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-21 00:16:09 +0200

    Added method to easily grab the first error code of validation or binding errors

--- back end/src/main/java/com/blogggr/utilities/SimpleBundleMessageSource.java
+++ back end/src/main/java/com/blogggr/utilities/SimpleBundleMessageSource.java
@@ -7,13 +7,20 @@ import org.springframework.lang.Nullable;
 /**
  * Created by Daniel Sunnen on 11.06.18.
  */
-public class SimpleBundleMessageSource extends ResourceBundleMessageSource{
+public class SimpleBundleMessageSource extends ResourceBundleMessageSource {
 
-  public String getMessage(String code){
+  public String getMessage(String code) {
     return getMessage(code, null, LocaleContextHolder.getLocale());
   }
 
-  public String getMessage(String code, @Nullable Object[] args){
+  public String getMessage(String code, @Nullable Object[] args) {
     return getMessage(code, args, LocaleContextHolder.getLocale());
+  }
+
+  public String getMessage(String[] codes) {
+    if (codes == null || codes.length < 1) {
+      return "";
+    }
+    return getMessage(codes[0], null, LocaleContextHolder.getLocale());
   }
 }
