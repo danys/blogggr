b7c092491da07a700490cff8ba8394b6669afee6
Daniel Sünnen <netfox2006@hotmail.com>
2018-06-24 15:13:10 +0200

    Separate enum class for user

---
+++ back end/src/main/java/com/blogggr/dto/UserEnums.java
@@ -0,0 +1,17 @@
+package com.blogggr.dto;
+
+/**
+ * Created by Daniel Sunnen on 24.06.18.
+ */
+public class UserEnums {
+
+  private UserEnums(){}
+
+  public enum Lang {
+    EN, DE, FR, LU
+  }
+
+  public enum Sex {
+    M, F
+  }
+}
