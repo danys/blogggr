300e84a3212a18313a5920083d680474d2832a90
Daniel Sünnen <netfox2006@hotmail.com>
2018-07-17 11:41:32 +0200

    Use logger

--- back end/src/main/java/com/blogggr/dao/GenericDaoImpl.java
+++ back end/src/main/java/com/blogggr/dao/GenericDaoImpl.java
@@ -25,26 +25,32 @@ public abstract class GenericDaoImpl<T extends Serializable> implements GenericD
   }
 
   public T findById(Long id) {
+    logger.debug("GenericDaoImpl - findById: {}, clazz: {}", id, clazz.getName());
     return entityManager.find(clazz, id);
   }
 
   public List<T> findAll() {
+    logger.debug("GenericDaoImpl - findAll, clazz: {}", clazz.getName());
     return entityManager.createQuery("from " + clazz.getName()).getResultList();
   }
 
   public void save(T entity) {
+    logger.debug("GenericDaoImpl - save, clazz: {}", clazz.getName());
     entityManager.persist(entity);
   }
 
   public T update(T entity) {
+    logger.debug("GenericDaoImpl - update, clazz: {}", clazz.getName());
     return entityManager.merge(entity);
   }
 
   public void delete(T entity) {
+    logger.debug("GenericDaoImpl - delete, clazz: {}", clazz.getName());
     entityManager.remove(entity);
   }
 
   public void deleteById(Long entityId) {
+    logger.debug("GenericDaoImpl - deleteById, clazz: {}", clazz.getName());
     T entity = findById(entityId);
     delete(entity);
   }
