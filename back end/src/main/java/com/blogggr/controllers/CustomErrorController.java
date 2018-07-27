b37a25400213d8e05b831d66fa0177baa19e06a2
Daniel Sünnen <netfox2006@hotmail.com>
2018-07-27 13:58:53 +0200

    Remove unused imports

--- back end/src/main/java/com/blogggr/controllers/CustomErrorController.java
+++ back end/src/main/java/com/blogggr/controllers/CustomErrorController.java
@@ -1,7 +1,5 @@
 package com.blogggr.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
