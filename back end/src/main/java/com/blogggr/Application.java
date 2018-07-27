bcea607856679c55bdb2a11a65f6131fa3089acd
Daniel Sünnen <netfox2006@hotmail.com>
2018-07-27 13:53:21 +0200

    Remove enablewebmvc

--- back end/src/main/java/com/blogggr/Application.java
+++ back end/src/main/java/com/blogggr/Application.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageConfig.class)
-@EnableWebMvc
 public class Application {
 
   public static void main(String[] args) {
