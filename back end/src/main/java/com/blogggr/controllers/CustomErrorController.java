36658dbc33cb711ad0f4c5aeedaf17ee6a6218d5
Daniel Sünnen <netfox2006@hotmail.com>
2018-07-27 14:00:02 +0200

    Remove path variable

--- back end/src/main/java/com/blogggr/controllers/CustomErrorController.java
+++ back end/src/main/java/com/blogggr/controllers/CustomErrorController.java
@@ -10,15 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomErrorController implements ErrorController {
 
-  private static final String PATH = "/error";
-
-  @RequestMapping(value = PATH)
+  @RequestMapping(value = "/error")
   public String error() {
     return "Error path not found!";
   }
 
   @Override
   public String getErrorPath() {
-    return PATH;
+    return "/error";
   }
 }
