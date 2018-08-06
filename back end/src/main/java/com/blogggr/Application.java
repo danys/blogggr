276cceedc3b5f7684b171d048062a779b843321d
Daniel Sünnen <netfox2006@hotmail.com>
2018-08-06 20:58:32 +0200

    Remove unused imports

--- back end/src/main/java/com/blogggr/Application.java
+++ back end/src/main/java/com/blogggr/Application.java
@@ -2,7 +2,6 @@ package com.blogggr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
--- back end/src/main/java/com/blogggr/config/AppConfig.java
+++ back end/src/main/java/com/blogggr/config/AppConfig.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
