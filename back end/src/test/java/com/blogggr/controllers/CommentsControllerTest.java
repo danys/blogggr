11969cd4396d3eb3c54e97d6a30f463eae79e6ee
Daniel Sünnen <netfox2006@hotmail.com>
2018-08-07 22:59:09 +0200

    Add empty test

---
+++ back end/src/test/java/com/blogggr/controllers/CommentsControllerTest.java
@@ -0,0 +1,19 @@
+package com.blogggr.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Daniel Sunnen on 07.08.18.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CommentsControllerTest {
+
+  @Test
+  public void createComment_Normal(){
+    //TODO
+  }
+}
