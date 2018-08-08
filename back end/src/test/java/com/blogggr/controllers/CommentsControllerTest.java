9a3f4bc174720b76bd262a972b3cc62bfe1bbd83
Daniel Sünnen <netfox2006@hotmail.com>
2018-08-08 23:27:56 +0200

    Added unit tests

--- back end/src/test/java/com/blogggr/controllers/CommentsControllerTest.java
+++ back end/src/test/java/com/blogggr/controllers/CommentsControllerTest.java
@@ -1,6 +1,7 @@
 package com.blogggr.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -90,5 +91,72 @@ public class CommentsControllerTest {
         .andExpect(status().isCreated())
         .andExpect(content().json("{'apiVersion': '1.0', 'data': null}"))
         .andExpect(header().string(AppConfig.LOCATION_HEADER_KEY,AppConfig.FULL_BASE_URL + "/comments/" + comment.getCommentId()));
+  }
+
+  @Test
+  public void updateComment_AccessDenied_Without_Authentication() throws Exception{
+    CommentData commentData = new CommentData();
+    mvc.perform(put(BASE_URL + "/comments/1", commentData)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isUnauthorized())
+        .andExpect(content().json("{'apiVersion': '1.0', 'errors': ['Access denied: Unauthenticated access not allowed!']}"));
+  }
+
+  @Test
+  public void updateComment_Normal() throws Exception{
+    Comment comment = new Comment();
+    comment.setCommentId(100L);
+    comment.setText("commentText");
+    doNothing().when(commentService).updateComment(any(Long.class),any(Long.class),any(CommentData.class));
+    CommentData commentData = new CommentData();
+    commentData.setPostId(1L);
+    commentData.setText("text");
+    mvc.perform(put(BASE_URL + "/comments/1")
+        .with(user(createUserPrincipal("dan@dan.com",1L)))
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(commentData)))
+        .andExpect(status().isOk())
+        .andExpect(content().json("{'apiVersion': '1.0', 'data': null}"));
+  }
+
+  @Test
+  public void deleteComment_AccessDenied_Without_Authentication() throws Exception{
+    CommentData commentData = new CommentData();
+    mvc.perform(delete(BASE_URL + "/comments/1", commentData)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isUnauthorized())
+        .andExpect(content().json("{'apiVersion': '1.0', 'errors': ['Access denied: Unauthenticated access not allowed!']}"));
+  }
+
+  @Test
+  public void deleteComment_Normal() throws Exception{
+    Comment comment = new Comment();
+    comment.setCommentId(100L);
+    comment.setText("commentText");
+    doNothing().when(commentService).deleteComment(any(Long.class),any(Long.class));
+    mvc.perform(delete(BASE_URL + "/comments/1")
+        .with(user(createUserPrincipal("dan@dan.com",1L))))
+        .andExpect(status().isOk())
+        .andExpect(content().json("{'apiVersion': '1.0', 'data': null}"));
+  }
+
+  @Test
+  public void getComment_AccessDenied_Without_Authentication() throws Exception{
+    mvc.perform(get(BASE_URL + "/comments/1")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isUnauthorized())
+        .andExpect(content().json("{'apiVersion': '1.0', 'errors': ['Access denied: Unauthenticated access not allowed!']}"));
+  }
+
+  @Test
+  public void getComment_Normal() throws Exception{
+    Comment comment = new Comment();
+    comment.setCommentId(100L);
+    comment.setText("text1");
+    when(commentService.getCommentById(any(Long.class),any(Long.class))).thenReturn(comment);
+    mvc.perform(get(BASE_URL + "/comments/1")
+        .with(user(createUserPrincipal("dan@dan.com",1L))))
+        .andExpect(status().isOk())
+        .andExpect(content().json("{'apiVersion': '1.0', 'data': {'commentId': 100, 'text': 'text1'}}"));
   }
 }
