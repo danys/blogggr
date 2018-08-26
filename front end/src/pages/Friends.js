11e7e326e64eff6296ba7856fa8d991b068ba31a
Daniel Sünnen <netfox2006@hotmail.com>
2018-08-26 18:22:29 +0200

    Length renamed

--- front end/src/pages/Friends.js
+++ front end/src/pages/Friends.js
@@ -22,7 +22,7 @@ class Friends extends React.Component{
                 firstName: '',
                 lastName: '',
                 email: '',
-                length: 10
+                maxRecordsCount: 10
             }
         };
         this.pagesInTransit={};
@@ -40,7 +40,7 @@ class Friends extends React.Component{
             const nextPageN = (pageN+1).toString();
             const previousPageN = (pageN-1).toString();
             if (this.state.friendsSearchData!=null && this.state.friendsSearchData.hasOwnProperty(previousPageN)){
-                requestParams.after = this.state.friendsSearchData[previousPageN].pageItems[this.state.searchParams.length-1].userId;
+                requestParams.after = this.state.friendsSearchData[previousPageN].pageItems[this.state.searchParams.maxRecordsCount-1].userId;
             } else if (this.state.friendsSearchData!=null && this.state.friendsSearchData.hasOwnProperty(nextPageN)){
                 requestParams.before = this.state.friendsSearchData[nextPageN].pageItems[0].userId;
             } else {
@@ -95,7 +95,7 @@ class Friends extends React.Component{
                         <Column
                             header={<InputHeaderCell field="First name" onChange={this.searchFormChange.bind(this, 'firstName')} />}
                             cell={props => (
-                                <TextCell {...props} loadUsers={this.fetchUsers} field='firstName' data={rowData} itemsPerPage={this.state.searchParams.length}/>
+                                <TextCell {...props} loadUsers={this.fetchUsers} field='firstName' data={rowData} itemsPerPage={this.state.searchParams.maxRecordsCount}/>
                             )}
                             width={130}
                         />
@@ -103,7 +103,7 @@ class Friends extends React.Component{
                             header={<InputHeaderCell field="Last name" onChange={this.searchFormChange.bind(this, 'lastName')} />}
                             cell={props => (
                                 <Cell {...props}>
-                                    <TextCell {...props} loadUsers={this.fetchUsers} field='lastName' data={rowData} itemsPerPage={this.state.searchParams.length}/>
+                                    <TextCell {...props} loadUsers={this.fetchUsers} field='lastName' data={rowData} itemsPerPage={this.state.searchParams.maxRecordsCount}/>
                                 </Cell>
                             )}
                             width={130}
@@ -112,7 +112,7 @@ class Friends extends React.Component{
                             header={<InputHeaderCell field="E-mail" onChange={this.searchFormChange.bind(this, 'email')} />}
                             cell={props => (
                                 <Cell {...props}>
-                                    <TextCell {...props} loadUsers={this.fetchUsers} field='email' data={rowData} itemsPerPage={this.state.searchParams.length}/>
+                                    <TextCell {...props} loadUsers={this.fetchUsers} field='email' data={rowData} itemsPerPage={this.state.searchParams.maxRecordsCount}/>
                                 </Cell>
                             )}
                             width={250}
